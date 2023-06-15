bafe0d4fd8b1b1a0578bbb900e203be64f1073fb
GamingadlerHD <121448226+GamingadlerHD@users.noreply.github.com>
2023-06-15 22:44:26 +0200

    Add files via upload

---
+++ migrations/models/77_20000000000001_update.sql
@@ -0,0 +1,4 @@
+-- upgrade --
+ALTER TABLE "special" ADD COLUMN rebels_card varchar(200);
+ALTER TABLE "special" ADD COLUMN monarchy_card varchar(200);
+ALTER TABLE "special" ADD COLUMN teretory_card varchar(200);
